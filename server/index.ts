10bf69be44e8fe4cd5a6971650728780381fe7c5
andvvo <121371226+andvvo@users.noreply.github.com>
2025-11-12 21:36:40 -0800

    Created backend

---
+++ server/index.ts
@@ -0,0 +1,13 @@
+import express, { Request, Response } from "express";
+import mongoose from "mongoose";
+
+const app = express();
+const PORT = 3000;
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello from TypeScript Express!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
