5a6b38e5b4e0af8666f80c7bcf98e277e1b60711
andvvo <121371226+andvvo@users.noreply.github.com>
2025-11-18 00:43:53 -0800

    Added responsive food in fridge

--- src/pages/cook.tsx
+++ src/pages/cook.tsx
@@ -20,19 +20,98 @@ export default function Cook() {
 
   return (
     <div className="flex justify-center items-center h-full w-full gap-6">
-      <div className="flex-1 flex-shrink-0 flex justify-end h-full w-full relative">
-        <img src={fridge} alt="Fridge" className="h-full object-contain" />
+      <div className="flex-1 flex-shrink-0 flex justify-end h-full w-full relative aspect-[30/41]">
+        <div className="relative w-full aspect-[30/41] -mr-20">
+          <img
+            src={fridge}
+            alt="Fridge"
+            className="absolute inset-0 w-full h-full object-contain pointer-events-none"
+          />
 
-        {/* <FoodIcon
-          defaultSrc={foodIcons.tomatoDark}
-          hoverSrc={foodIcons.tomatoLight}
-          selectedSrc={foodIcons.tomatoLight}
-          selected={isSelected("tomato")}
-          onToggle={() => toggleFood("tomato")}
-          alt="Tomato"
-          // className="absolute top-0 left-0 translate-x-40 translate-y-40 w-24 h-auto"
-          className="absolute inset-65 w-[16%] h-auto"
-        /> */}
+          <FoodIcon
+            defaultSrc={foodIcons.tomatoDark}
+            hoverSrc={foodIcons.tomatoLight}
+            selectedSrc={foodIcons.tomatoLight}
+            selected={isSelected("tomato")}
+            onToggle={() => toggleFood("tomato")}
+            alt="Tomato"
+            className="absolute left-[41%] top-[26%] w-[14%] -translate-x-1/2 -translate-y-1/2 z-10"
+          />
+          <FoodIcon
+            defaultSrc={foodIcons.onionDark}
+            hoverSrc={foodIcons.onionLight}
+            selectedSrc={foodIcons.onionLight}
+            selected={isSelected("onion")}
+            onToggle={() => toggleFood("onion")}
+            alt="Onion"
+            className="absolute left-[55%] top-[26%] w-[14%] -translate-x-1/2 -translate-y-1/2 z-10"
+          />
+          <FoodIcon
+            defaultSrc={foodIcons.milkDark}
+            hoverSrc={foodIcons.milkLight}
+            selectedSrc={foodIcons.milkLight}
+            selected={isSelected("milk")}
+            onToggle={() => toggleFood("milk")}
+            alt="Milk"
+            className="absolute left-[69%] top-[26%] w-[14%] -translate-x-1/2 -translate-y-1/2 z-10"
+          />
+
+          <FoodIcon
+            defaultSrc={foodIcons.lettuceDark}
+            hoverSrc={foodIcons.lettuceLight}
+            selectedSrc={foodIcons.lettuceLight}
+            selected={isSelected("lettuce")}
+            onToggle={() => toggleFood("lettuce")}
+            alt="Lettuce"
+            className="absolute left-[41%] top-[45%] w-[14%] -translate-x-1/2 -translate-y-1/2 z-10"
+          />
+          <FoodIcon
+            defaultSrc={foodIcons.garlicDark}
+            hoverSrc={foodIcons.garlicLight}
+            selectedSrc={foodIcons.garlicLight}
+            selected={isSelected("garlic")}
+            onToggle={() => toggleFood("garlic")}
+            alt="Garlic"
+            className="absolute left-[55%] top-[46%] w-[14%] -translate-x-1/2 -translate-y-1/2 z-10"
+          />
+          <FoodIcon
+            defaultSrc={foodIcons.chickenDark}
+            hoverSrc={foodIcons.chickenLight}
+            selectedSrc={foodIcons.chickenLight}
+            selected={isSelected("chicken")}
+            onToggle={() => toggleFood("chicken")}
+            alt="Chicken"
+            className="absolute left-[69%] top-[46%] w-[14%] -translate-x-1/2 -translate-y-1/2 z-10"
+          />
+
+          <FoodIcon
+            defaultSrc={foodIcons.cheeseDark}
+            hoverSrc={foodIcons.cheeseLight}
+            selectedSrc={foodIcons.cheeseLight}
+            selected={isSelected("cheese")}
+            onToggle={() => toggleFood("cheese")}
+            alt="Cheese"
+            className="absolute left-[41%] top-[64%] w-[14%] -translate-x-1/2 -translate-y-1/2 z-10"
+          />
+          <FoodIcon
+            defaultSrc={foodIcons.breadDark}
+            hoverSrc={foodIcons.breadLight}
+            selectedSrc={foodIcons.breadLight}
+            selected={isSelected("bread")}
+            onToggle={() => toggleFood("bread")}
+            alt="Bread"
+            className="absolute left-[55%] top-[64%] w-[14%] -translate-x-1/2 -translate-y-1/2 z-10"
+          />
+          <FoodIcon
+            defaultSrc={foodIcons.eggDark}
+            hoverSrc={foodIcons.eggLight}
+            selectedSrc={foodIcons.eggLight}
+            selected={isSelected("egg")}
+            onToggle={() => toggleFood("egg")}
+            alt="Egg"
+            className="absolute left-[69%] top-[65%] w-[14%] -translate-x-1/2 -translate-y-1/2 z-10"
+          />
+        </div>
       </div>
       <div className="flex-1 flex-shrink-0 flex flex-col justify-around items-start h-full w-full relative">
         <img
